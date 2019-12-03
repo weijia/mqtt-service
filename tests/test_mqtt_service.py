4d6a7e4addf612e22adbff4bb1c0795be49ca7d4
Richard <richard.j.wang@nokia-sbell.com>
2019-12-03 14:53:34 +0800

    Added tests.

--- tests/test_mqtt_service.py
+++ tests/test_mqtt_service.py
@@ -2,15 +2,23 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `mqtt_service` package."""
-
-
+import time
 import unittest
 from click.testing import CliRunner
 
 from mqtt_service import mqtt_service
 from mqtt_service import cli
+from mqtt_service.mqtt_base_device import MqttBaseDevice
+from mqtt_service.mqtt_base_listener import MqttBaseListener
+from mqtt_service.mqtt_multi_topic_listener import MqttMultiTopicListener
 
 
+class OutputForTopicListener(MqttMultiTopicListener):
+    def on_message(self, client, user_data, msg):
+        print(client, user_data, msg)
+
+
+# noinspection PyPep8Naming
 class TestMqtt_service(unittest.TestCase):
     """Tests for `mqtt_service` package."""
 
@@ -22,6 +30,16 @@ class TestMqtt_service(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
+        server = 'hz4labs04.china.nsn-net.net'
+        s = OutputForTopicListener(server, ['/test/#'])
+        s.loop_start()
+        # s.client.publish("/test/", "bad")
+        c = MqttBaseDevice(server)
+        c.loop_start()
+        while not c.client.is_connected():
+            pass
+        c.client.publish("/test/", "good")
+        time.sleep(5)
 
     def test_command_line_interface(self):
         """Test the CLI."""
